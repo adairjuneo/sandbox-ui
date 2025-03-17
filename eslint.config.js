574c244535f225a0778ff76c09276c036c0d7e06
Adair Juneo <adair_juneo@hotmail.com>
2025-03-17 10:39:24 -0300

    install prettier and add more eslint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+      eslintPluginPrettier,
+    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,6 +24,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +32,49 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: 'req|res|next|__',
+          destructuredArrayIgnorePattern: '__',
+        },
+      ],
+      semi: ['warn', 'always'],
+      'semi-spacing': 'error',
+      quotes: ['warn', 'single'],
+      'no-console': ['warn', { allow: ['error', 'info'] }],
+      'no-undef': 'error',
+      'arrow-spacing': 'error',
+      'no-confusing-arrow': 'error',
+      'no-duplicate-imports': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'off',
+      'prefer-const': 'error',
+      'prefer-template': 'warn',
+      eqeqeq: 'warn',
+      'no-invalid-this': 'error',
+      'no-return-assign': 'error',
+      'no-unused-expressions': ['error', { allowTernary: true }],
+      'no-useless-concat': 'error',
+      'no-useless-return': 'error',
+      'no-constant-condition': 'warn',
+      'no-mixed-spaces-and-tabs': 'warn',
+      'space-before-blocks': 'error',
+      'space-in-parens': 'error',
+      'space-infix-ops': 'error',
+      'space-unary-ops': 'error',
+      'keyword-spacing': 'error',
+      'multiline-ternary': ['error', 'never'],
+      'no-mixed-operators': 'error',
+      'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+      'no-whitespace-before-property': 'error',
+      'object-property-newline': [
+        'error',
+        { allowAllPropertiesOnSameLine: true },
+      ],
     },
-  },
-)
+    ignores: ['dist/**', 'build/**', '.vscode/*', '.env', 'node_modules'],
+  }
+);
---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false,
+};
